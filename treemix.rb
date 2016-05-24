122c2220cfccc569e3912068afc2144acba4f171
tmbogus <jwgaiarsa@usp.br>
2016-05-24 13:29:05 -0300

    First commit

---
+++ treemix.rb
@@ -0,0 +1,21 @@
+class Treemix < Formula
+  desc "TreeMix is a method for inferring the patterns of population splits and mixtures in the history of a set of populations. In the underlying model, the modern-day populations in a species are related to a common ancestor via a graph of ancestral populations. We use the allele frequencies in the modern populations to infer the structure of this graph."
+  homepage "http://pritchardlab.stanford.edu/software.html"
+  url "https://bitbucket.org/nygcresearch/treemix/downloads/treemix-1.12.tar.gz"
+  version "1.12"
+  sha256 "28ca3b9ac85fa2a8432f1856123b75e7e845eb38052378439d6bbbe38e1d60e6"
+
+  depends_on "gsl"
+
+  def install
+    #ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+end
