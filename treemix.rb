ad89df75e499858d73e1e329ed32acd3d526ea92
tmbogus <jwgaiarsa@usp.br>
2016-05-24 13:40:38 -0300

    Formula comment cleanup commit

--- treemix.rb
+++ treemix.rb
@@ -8,14 +8,11 @@ class Treemix < Formula
   depends_on "gsl"
 
   def install
-    #ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
 end
